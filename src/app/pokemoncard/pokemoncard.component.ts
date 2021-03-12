d9947fa3a58da2bfef4dfbc75eeaccee243fc08c
okarp <ky6034@kolumbus.fi>
2021-03-12 12:00:52 +0200

    feat: add functionality for 'catching' pokemon

--- src/app/pokemoncard/pokemoncard.component.ts
+++ src/app/pokemoncard/pokemoncard.component.ts
@@ -14,14 +14,26 @@ export class PokemoncardComponent implements OnInit {
 
   pokemon!: Pokemon;
   loaded : boolean = false; 
+  imageSource: string = '';
+  caught: boolean = false;
 
   constructor(private apiFetcher: PokemonapifetcherService, private route: ActivatedRoute) { }
 
 
   ngOnInit() {         
       this.apiFetcher.fetchApi(this.route.snapshot.paramMap.get('id')!).subscribe((data: Pokemon)=>{      
-      this.pokemon = data;      
+      this.pokemon = data;
+      this.imageSource = this.pokemon.sprites.front_default;      
       this.loaded = true;        
     })  
+  }
+
+  catchPokemon(){
+    console.log("catch")
+    this.imageSource = 'assets/images/pokeball.gif'
+    this.caught = true;
+    setTimeout( ()=>{
+      this.imageSource = this.pokemon.sprites.front_default;
+      }, 5000)
   }
 }
